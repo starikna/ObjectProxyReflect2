6c0a00c46c88b1de434abed90a25be05afdbe39f
starikna <don.strekoza@gmail.com>
2023-03-20 00:59:27 +0500

    First commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,3 @@
-// const character = {
-//   name: 'Лучник',
-//   type: 'Bowman',
-//   health: 50,
-//   level: 3,
-//   attack: 40,
-//   defence: 10,
-//   special: [
-//     {
-//       id: 8,
-//       name: 'Двойной выстрел',
-//       icon: 'http://...',
-//       description: 'Двойной выстрел наносит двойной урон'
-//     }, 
-//     {
-//       id: 9,
-//       name: 'Нокаутирующий удар',
-//       icon: 'http://...'
-//       // <- обратите внимание, описание "засекречено"
-//     }
-//   ]	
-// }
-
 export default function destructionObj({
   special: [{
     id,
